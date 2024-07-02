6e53d60615d4bab8cea1ef295d120ee5a9cccb02
natuliochek <natvicor@mail.ru>
2024-07-02 16:35:56 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -4,32 +4,44 @@ import './App.css';
 
 function App() {
   const [item, setItem] = useState(data);
+  const [showText, setShowText] = useState(false);
+
   const removeItem = (id) => {
-    console.log(id)
+
     let newItemList = item.filter(item => item.id !== id);
-    console.log(newItemList)
+
     setItem(newItemList)
   }
 
+  const showTextClick = (item) => {
+    item.showMore = !item.showMore
+    setShowText(!showText)
+  }
+  
   return (
     <div>
       <div className='container'>
-        <h1>List of { item.length } MayBeToBuy</h1>
+        <h1>List of { item.length } MayBeToBuy Things</h1>
       </div>
 
       {item.map((item => {
-        const {id, name, image, price} = item;
+        const {id, name, image, price, description, showMore} = item;
 
         return(
       <div>
         <div className='container'>
-          <img src={ image } width="200px" alt="item"/>
-        </div>
-        <div className='container'>
           <h2>{ name }</h2>
         </div>
         <div className='container'>
-          <p>{ price }</p>
+          <img src={ image } width="200px" alt="item"/>
+        </div>
+        <div className='container'>
+          <p className='describtion'>{showMore ? description : description.substring(0,50) + "..."}
+            <button className='btnShow' onClick={()=> showTextClick(item)} >{ showMore ? "Show less" : "Show more" }</button>
+          </p>
+        </div>
+        <div className='container'>
+          <p className='price'>{ price }</p>
         </div>
         <div className='container'>
           <button className='btn' onClick={()=> removeItem(id)}>Remove</button>
--- src/data.js
+++ src/data.js
@@ -2,115 +2,153 @@ export const data = [
     {
         id: 1,
         name: "Duschvorhangringe",
+        description:"Rostfreies Material: Die Duschvorhangringe bestehen zu 100 % aus Edelstahl 304, sind rost- und korrosionsbeständig und rosten im Außen- und Innenbereich sowie in feuchten Badezimmerumgebungen nicht für den Langzeitgebrauch. / Sanftes Gleiten: Jeder Duschvorhang haken enthält 5 perfekt abgerundete Kugeln, die eine extrem sanfte Hin- und Herzzeitbewegung ermöglichen. Die Haken sind stark genug, um einen schweren Vorhang zu halten. / Eleganter Look: Glänzendes Duschringe aus Metall sind galvanisiert und poliert, schön und großzügig und verblassen auch nach längerem Gebrauch nicht. Die dekorativen Duschhaken passen zu allen Arten von Duschen Vorhänge. / Einfach zu installieren: Die Rostfrei Aufhängeringe sind einfach zu installieren, drücken Sie, um die Sperre zu lösen, legen Sie die Vorhanghaken in Stangen, hängen Sie den Vorhang auf Haken, dann sperren Sie die Haken. / Paket beinhaltet: Set mit 20 Stück Vorhang-Ringe, aus Edelstahl, Goldfarbene, seitliche Öffnung, Rollen-Gleitsystem, Durchmesser von 7,2 x 4,1 cm",
         image: "https://m.media-amazon.com/images/I/71xlj8TG3wL._AC_SX679_.jpg",
-        price: "6,49€"
+        price: "6,49€",
+        showMore: false
     },
     {
         id: 2,
         name: "Kérastase Shampoo für langes und geschädigtes Haar",
+        description:"Die Pflegeserie Extentioniste richtet sich an Frauen, die ihr Haarwachstum anregen und gleichzeitig Haarbruch und Spliss vorbeugen möchten, um schönes, langes und widerstandsfähiges Haar zu erhalten.",
         image: "https://m.media-amazon.com/images/I/61FZ4deayOL._SX522_.jpg",
-        price: "36,75€"
+        price: "36,75€",
+        showMore: false
     },
     {
         id: 3,
         name: "Kérastase Kopfhaut Sérum Spécifique",
+        description:"Die Kopfhaut wird ins Gleichgewicht gebracht, beruhigt und die Schutzbarriere vor äußeren Einflüssen gestärkt. Die Haarpflege hinterlässt ein frisches Gefühl und hebt die Haarwurzeln an - für eine gesunde Kopfhaut und schönes Haar.",
         image: "https://m.media-amazon.com/images/I/51zzJMVMPRL._SX522_.jpg",
-        price: "33,74€"
+        price: "33,74€",
+        showMore: false
     },
     {
         id: 4,
-        name: "Kérastase Sérum Anti-Chute Fortifiant",
+        name: "Kérastase Sérum Fortifiant",
+        description:"Es stärkt die Haarfaser von den Wurzeln bis zu den Spitzen und aktiviert die Mikrozirkulation von Nährstoffen, um das Wachstum von gesundem Haar anzuregen.",
         image: "https://m.media-amazon.com/images/I/61pz1I2MlJL._SX522_.jpg",
-        price: "37,04€"
+        price: "37,04€",
+        showMore: false
     },
     {
         id: 5,
-        name: "Teekanne Glas 1,2 Liter",
+        name: "Teekanne 1,2 Liter",
+        description:"Die kristallklare Glas Teekanne mit Sieb ist stabil und trotzdem leicht und ideal für die Verwendung mit Blütentees. Wir sind noch einen Schritt weiter gegangen und haben die Glaskanne mit einem extra weiten Henkel und einer nicht tropfenden Tülle ausgestattet. Die großzügige Kapazität von 1,2 Litern erlaubt das Aufbrühen von bis zu vier Tassen Tee.",
         image: "https://m.media-amazon.com/images/I/8155TWskgdL._AC_SX679_.jpg",
-        price: "21,21€"
+        price: "21,21€",
+        showMore: false
     },
     {
         id: 6,
-        name: "Kérastase Hitzeschutzspray für geschwächtes Haar Défense Thermique",
+        name: "Kérastase Hitzeschutzspray Défense Thermique",
+        description:"Hitzeschutzspray für geschwächtes Haar Défense Thermique verringert das Risiko von Haarausfall, indem es die Haarfaser stärkt und vor Hitze bis zu 220 °C schützt. Das Haar wird veredelt und erhält neuen Glanz und Geschmeidigkeit.",
         image: "https://m.media-amazon.com/images/I/61-yCfnCvXL._SX522_.jpg",
-        price: "26,59€"
+        price: "26,59€",
+        showMore: false
     },
     {
         id: 7,
         name: "Kerastase Specifique Masque Hydra-Apaisant",
+        description:"	Hydratisiert und beruhigt die Kopfhaut, um die Hautqualität zu verbessern und der Kopfhaut zu helfen, Trockenheit und Fettigkeit zu bekämpfen.Hydratisiert und beruhigt die Kopfhaut, um die Hautqualität zu verbessern und der Kopfhaut zu helfen, Trockenheit und Fettigkeit zu bekämpfen. Eine leichte Gelmaske, die den essentiellen Feuchtigkeitsgehalt des Haares sofort wieder ins Gleichgewicht bringt, die Haarfaser nährt und die Haarqualität wiederherstellt.",
         image: "https://m.media-amazon.com/images/I/5129lByU9CL._SX425_.jpg",
-        price: "41,90€"
+        price: "41,90€",
+        showMore: false
     },
     {
         id: 8,
-        name: "Kérastase Haarmaske für langes und geschädigtes Haar, Masque Extentioniste, Résistance",
+        name: "Kérastase Haarmaske Résistance",
+        description:"Die Masque Extentioniste Haarmaske kurbelt das Haarwachstum an und kräftigt es, sodass es in drei Monaten bis zu 4 cm an Länge gewinnt. Die Formel mit Kreatin, Ceramiden und Maleinsäure wirkt im Kern der Haarfaser und repariert sie, wodurch Spliss und Haarbruch reduziert werden. Es stärkt die Faserstruktur, repariert die Kutikula des Haares und schützt das Haar von den Längen bis zu den Spitzen vor schädlichen Umwelteinflüssen. Eine walnussgroße Menge des Produkts auf das handtuchtrockene Haar auftragen. Die Maske auf den Längen und Spitzen verteilen. 5 Minuten einwirken lassen und dann gründlich ausspülen",
         image: "https://m.media-amazon.com/images/I/51vLI5Iz86L._SX425_.jpg",
-        price: "39,08€"
+        price: "39,08€",
+        showMore: false
     },
     {
         id: 9,
-        name: "Teeblumen Variation 8 Teeblüten in 4 verschiedenen Sorten (Weißer Tee)",
+        name: "Teeblumen Variation",
+        description:"Bestandteile: Weißer Tee (min. 95%); Blüten (1-5%) von Ringelblume, Rose (Damascena), Jasmin, Rose (Centifolia), Aroma (Vanille,Jasmin,Zitrone,Pfirsich) / Gebrauchsanweisung / Zubereitung Schritt 1: Ca. 500ml Wasser aufkochen und unverzüglich in die Glasktasse geben. Bitte beachten: Im Gegensatz zur Grünteezubereitung ist hier die Verwendung von kochend heißem Wasser notwendig, um eine optimale Öffnung der ErblühTee-Kugeln zu gewährleisten. Schritt 2: ErblühTee-Teelini vorsichtig ins Wasser geben und staunen... Schritt 3: Bitte die empfohlene Ziehdauer beachten. Es sind 2-3 Aufgüsse möglich, die in Ihrer Geschmacksintensität variieren können. Verpackungsinformation ErblühTeelini: Jede Teelinikugel ist seperat im dekorativen Tetraeder folienverschweißt, sodass eine lange Haltbarkeit und hohe Qualität in Frische und Geschmack gewährleistet ist.",
         image: "https://m.media-amazon.com/images/I/71NC1OGLiaL._SX522_.jpg",
-        price: "14,95€"
+        price: "14,95€",
+        showMore: false
     },
     {
         id: 10,
-        name: "Deko Korb, Metall, ∅ 22 cm, gold",
+        name: "Deko Korb ∅ 22 cm",
+        description:"Praktisch: Der Aufbewahrungskorb bietet Stauraum für Gegenstände aller Art - Korb in glänzendem Gold......Flexibel: Allzweckkorb ist vielseitig verwendbar in Wohn-, Ankleide-, Schlaf- o. Arbeitszimmer etc...Basics: Geräumiger Metallkorb mit dekorativen geometrischen Maschen - Maße H x D: ca. 12,5 x 22 cm. Ordnung: Mit dem Drahtkorb übersichtlich Kleinkram im Regal, Schrank oder auf dem Tisch verstauen. Design: Rundes Körbchen in Vintage Optik bewahrt Kleidung, Kosmetikartikel, Deko uvm. stilvol",
         image: "https://m.media-amazon.com/images/I/51cW7TdP4GL._MCnd_AC_.jpg",
-        price: "12,99€"
+        price: "12,99€",
+        showMore: false
     },
     {
         id: 11,
-        name: "Götz Hannah Puppe 50 cm Ballerina Stehpuppe mit schwarzen Langen Haaren und braunen Augen 15-teiliges Set",
+        name: "Götz Hannah Ballerina",
+        description: "Hannah (ca. 820 g) Ballerina Stehpuppe mit schwarzen Langen Haaren und braunen Augen 15-teiliges Set. Hannah hat ihren ersten großen Ballettauftritt. Dazu bekam sie natürlich ein Tutu. Ihr Ballettanzug besteht aus einer Wickeljacke, einem Body, Strumpfhosen und selbstverständlich Ballerinas und das Tutu. Im Haar trägt sie ihren Lieblingszopfhalter, eine große Blume aus Pailletten. 50 cm Hartkörperpuppe, schwarze lange waschbare Haare, braune Augen, Gelenkfunktion und natürliche Körper-Proportionen",
         image: "https://m.media-amazon.com/images/I/71NC1OGLiaL._SX522_.jpg",
-        price: "Derzeit nicht verfügbar"
+        price: "Derzeit nicht verfügbar",
+        showMore: false
     },
     {
         id: 12,
-        name: "Götz Happy Kidz Sophie geht zum Ballett Puppe 50 cm Multigelenk-Stehpuppe, braune Haare, braune Augen 9-teiliges Set",
+        name: "Götz Happy Kidz Sophie",
+        description: "Sophie (ca. 690 g) geht zum Ballett Puppe 50 cm Multigelenk-Stehpuppe, braune Haare, braune Augen 9-teiliges Set. Dazu hat sie schon ihren Body an, ihre Ballettjacke und ein Tutu und um das Outfit perfekt zu machen trägt sie noch Stulpen über die Strumpfhose und ihre Ballettschühchen hat sie ebenso angezogen.Hast du auch schon die Sporttasche gepackt? Dann nichts wie los und viel Spaß! 50 cm Hartkörper-Puppe, braune waschbare Haare, braune Augen, Multigelenkfunktion",
         image: "https://m.media-amazon.com/images/I/51IocI4kJlL._AC_SX679_.jpg",
-        price: "99,99€"
+        price: "99,99€",
+        showMore: false
     },
     {
         id: 13,
-        name: "Götz Just Like me - Mia im Bienenoutfit Puppe - 27 cm Lange blonde Haare, blaue Schlafaugen 7-teiligen Set",
+        name: "Götz Just Like me - Mia",
+        description: "Mia im Bienenoutfit Puppe - 27 cm Lange blonde Haare, blaue Schlafaugen 7-teiligen Set. Mia (ca. 300 g) weiß auch, wie wichtig sie für uns sind, daher trägt sie heute dieses schicke Outfit. Das T-Shirt mit Glitzer-Bienenaufdruck, dazu ein Röckchen mit schillerndem Satin und darüber zwei Schichten aus Tüll, die auch fein glitzern. Ganz witzig sind ihr ihre Bienenstiefel in gold-schwarz und einem süßen Bienengesicht. Für den Kopf hat sie ein edles Satin-Haarband ausgewählt und so geht sie richtig schick aus, 27 cm Hartkörper-Puppe, waschbare blonde lange Haare, blaue Schlafaugen, voll beweglicher Vinylkörper",
         image: "https://m.media-amazon.com/images/I/61m2QCSnv+L._AC_SY300_SX300_.jpg",
-        price: "39,99€"
+        price: "39,99€",
+        showMore: false
     },
     {
         id: 14,
-        name: "Götz Happy Kidz Sophia Puppe 50 cm Multigelenk blonde Haare blauen Augen 7-teiliges Set",
+        name: "Götz Happy Kidz Sophia",
+        description: "Sophia (ca. 690 g) Puppe 50 cm Multigelenk blonde Haare blauen Augen 7-teiliges Set. Sie trägt ein wunderschönes Kleid mit schillerndem Blumenmotiv am Oberteil und das Unterteil besteht aus reichlich Tüll, auch hier sind tolle Blüten aufgedruckt. Unter dem Kleid hat sie eine passende Strumpfhose und schicke Boots in schwarz. Der samtige Blazer passt zum Samtband am Kleid. Mit ihren großen blauen Augen hält sie Ausschau nach ihrer Freundin, 50 cm Hartkörper-Puppe, blonde waschbare Haare, blaue Augen, Multigelenkfunktion",
         image: "https://m.media-amazon.com/images/I/51Uezf9Yw2L._AC_SX679_.jpg",
-        price: "159,99€"
+        price: "159,99€",
+        showMore: false
     },
     {
         id: 15,
-        name: "Götz Just Like me - Lucia Zauberhexe Puppe 27 cm Langen blonden rot gesträhnte Haare, braune Schlafaugen 6-teiligen Set",
+        name: "Götz Just Like me - Lucia",
+        description: "Lucia (ca. 300 g) Zauberhexe Puppe 27 cm Langen blonden rot gesträhnte Haare, braune Schlafaugen 6-teiligen Set. Sie kommt mit coolem bunt-gesträhntem Haar und Glitzerkleid mit fedrigen Ärmelenden, dazu die schwarzen Stiefel, doch besonderes Highlight ist die Fledermaus im Haar, 27 cm Hartkörper-Puppe, waschbare blonde lange Haare mit roten Strähnchen, braune Schlafaugen, voll beweglicher Vinylkörper",
         image: "https://m.media-amazon.com/images/I/71CUXmlj4zL._AC_SY879_.jpg",
-        price: "39,99€"
+        price: "39,99€",
+        showMore: false
     },
     {
         id: 16,
-        name: "Zehensocken Damen Fünf Finger Socken aus Baumwolle, 5 Paare",
+        name: "Zehensocken Damen",
+        description:"1. weich und atmungsaktiv Material: 78% Baumwolle / 20% Elasthan / 2% Elastik......Hochwertige Baumwolle für überlegene Weichheit und Feuchtigkeitstransport, Elasthan für hervorragende Dehnbarkeit. Weich und hautfreundlich, mit Elastizität, schweißableitend, weich. 2. Hohe Elastizität: Ausgezeichnete Dehnbarkeit, Elasthan für bessere Passform. EU 36-41.",
         image: "https://m.media-amazon.com/images/I/7171vx2apbL._AC_SX522_.jpg",
-        price: "21,98€"
+        price: "21,98€",
+        showMore: false
     },
     {
         id: 17,
-        name: "Zehensocken Damen Fünf Finger Socken, 5 Paare",
+        name: "Zehensocken Damen",
+        description:"Material: 80% Baumwolle + 10% Elasthan + 10% Elastischer, hoher Baumwollstoff für hervorragende Weichheit und Feuchtigkeitsregulierung. Mesh-Strick: Atmungsaktiv, Schnelltrocknend, Schweißmindernd, dass sich Ihre Füße frisch und behaglich fühlen! Diese Damen Zehensocken sind für Frühling, Herbst, Winter geeignet.",
         image: "https://m.media-amazon.com/images/I/71VX1xq1lNL._AC_SX522_.jpg",
-        price: "14,98€"
+        price: "14,98€",
+        showMore: false
     },
     {
         id: 18,
-        name: "Großes geometrisches Terrarium, goldfarbenes Messing, rechteckige Form mit Fuß, Andenken",
+        name: "Großes geometrisches Terrarium",
+        description:"Hübsches Bild: aus verstärktem Glas, robust und sicher. Es wird nicht leicht brechen. Handgefertigt und vor dem Verpacken zu 100% überprüfen. Dies ist ein rechteckiges Terrarium mit Schwingdeckel. Ideal als Mittelpunkt auf Ihrem Tisch. Es ist auch ideal für Hochzeitskartenbox.【Perfektes Geschenk】Viele Menschen lieben es, ihre verträumte Welt zu erschaffen, oder eine Märchengeschichte innerhalb eines Terrariums. Es ist auch ideal für Umschläge, Empfang, Wishwell, Tafelaufsatz, Dekoration, Hochzeitsempfänge, Pflanzgefäßhalter, Geschenk und Präsentationsbox, Andenken-Mittelstück. Einzigartiges Design: Entworfen im Raumstil, bietet es ein Miniatur-Gartenblumenhaus aus Glas. Großes, goldenes, rechteckiges geometrisches Terrarium, mit Schwingdeckel, ideal als Tafelaufsatz, Vitrine. Es ist auch ideal für Glaskartenhalter auf einem Empfangstisch für Hochzeit oder Geburtstagsfeier Kartensammlung.【Premium Qualität & Größe】Artikelgröße:25,4 cm Länge, 19,6 cm Höhe, 14,2 cm Breite. Das Paket ist verdickt, um einen sicheren Transport zu gewährleisten. Die Oberfläche des Produkts ist sauber und ordentlich, leicht und belüftet.",
         image: "https://m.media-amazon.com/images/I/71fhRfPBnJL._AC_SX679_.jpg",
-        price: "56,64€"
+        price: "56,64€",
+        showMore: false
     },
     {
         id: 19,
-        name: "5 Paar orthopädische Kompressionssocken für Damen, voller Zehenbereich, atmungsaktiv, Yoga-Socken, mit Gel-Lasche",
+        name: "Feliciay 4 Paar Zehensocken",
+        description:"- Atmungsaktivität......Hergestellt aus hochwertigem Material, weich und atmungsaktiv, fühlt es sich im Sommer nicht heiß an und bringt Kühle an Ihre Füße. - Gemütlich......Fünf Zehen getrennt, Gesundheit und Hygiene, verhindern die Übertragung von Krankheiten. - Anti-Rutsch......Das Ultra-Low-Futter der Damen-Zehensocken für orthopädische Kompressionsstrümpfe ist mit einer Gelfolie ausgestattet, um ein leichtes Abfallen der Ferse beim Tragen zu verhindern. - Hochwertiges Design......Das ergonomische Design minimiert die Reibung zwischen den Zehen. Es hat Atmungsaktivität, Elastizität und Langlebigkeit, was die Reibung effektiv reduzieren und das Risiko von Blasen verringern kann. Spezifikationen: Material: Baumwolle und Acryl / Farbe: hautfarben /Geeignete Jahreszeit: Sommer, Frühling, Herbst, Winter / Funktion: Feuchtigkeitsaufnahme, Schweiß / Größe: Fit UK Damengröße Schuhgröße 2-6, Fit Europe Damengröße Schuhgröße 34-39.",
         image: "https://m.media-amazon.com/images/I/61loNBt7PlS._AC_SX522_.jpg",
-        price: "11,99€"
+        price: "10,99€",
+        showMore: false
     }
 ]
